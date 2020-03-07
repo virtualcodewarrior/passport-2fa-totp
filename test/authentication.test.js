af708607b8d0de877c7e6d540aac5409966ca707
Rodney Draaisma <virtualcodewarrior@hotmail.com>
2020-03-07 11:43:52 -0500

    modernize somewhat, add issuer fix no totp behavior

--- test/authentication.test.js
+++ test/authentication.test.js
@@ -1,26 +1,26 @@
 'use strict';
 
-var chai = require('chai'),
-    expect = chai.expect,
-    totp = require('notp').totp,
-    Strategy = require('..').Strategy;
-    
+const chai = require('chai');
+const expect = chai.expect;
+const totp = require('notp').totp;
+const Strategy = require('..').Strategy;
+
 chai.use(require('chai-passport-strategy'));
 
-var ERROR_USERNAME = 'error';
-var USERNAME = 'us3r';
-var PASSWORD = 'p4ssw0rd';
-var WRONG_PASSWORD = 'w40ng_pwd';
-var AUTH_FAILED = 'Invalid username or password';
-var ERROR = 'Error';
+const ERROR_USERNAME = 'error';
+const USERNAME = 'us3r';
+const PASSWORD = 'p4ssw0rd';
+const WRONG_PASSWORD = 'w40ng_pwd';
+const AUTH_FAILED = 'Invalid username or password';
+const ERROR = 'Error';
 
 describe('Authentication using TOTP', function () {
-    
+
     // Prepare test TOTP code (6 minutes period)
-    var secret = '12345678901234567890';
-    var totpCode = totp.gen(secret, { window: 6, time: 300 });
-    
-    var strategy = new Strategy(function (username, password, done) {
+    const secret = '12345678901234567890';
+    const totpCode = totp.gen(secret, { window: 6, time: 300 });
+
+    const strategy = new Strategy(function (username, password, done) {
         if (username === USERNAME && password === PASSWORD) {
             done(null, { username: username });
         } else {
@@ -29,15 +29,15 @@ describe('Authentication using TOTP', function () {
     }, function (user, done) {
         done(null, secret, 300);
     });
-        
+
     describe ('authentication - OK', function () {
-        var user;
-            
+        let user;
+
         before(function (done) {
             chai.passport.use(strategy)
                 .success(function (u) {
                     user = u;
-                    done();    
+                    done();
                 })
                 .req(function (req) {
                     req.body = {};
@@ -45,22 +45,22 @@ describe('Authentication using TOTP', function () {
                     req.body.password = PASSWORD;
                     req.body.code = totpCode;
                 })
-                .authenticate();    
+                .authenticate();
         });
-        
+
         it('check username', function () {
             expect(user.username).to.be.equal(USERNAME);
         });
     });
-    
+
     describe ('authentication - wrong password', function () {
-        var info;
-            
+        let info;
+
         before(function (done) {
             chai.passport.use(strategy)
                 .fail(function (i) {
                     info = i;
-                    done();    
+                    done();
                 })
                 .req(function (req) {
                     req.body = {};
@@ -68,22 +68,22 @@ describe('Authentication using TOTP', function () {
                     req.body.password = WRONG_PASSWORD;
                     req.body.code = totpCode;
                 })
-                .authenticate();    
+                .authenticate();
         });
-        
+
         it('check error message', function () {
             expect(info).to.be.equal(AUTH_FAILED);
         });
     });
-    
+
     describe ('authentication - wrong TOPT code', function () {
-        var info;
-            
+        let info;
+
         before(function (done) {
             chai.passport.use(strategy)
                 .fail(function (i) {
                     info = i;
-                    done();    
+                    done();
                 })
                 .req(function (req) {
                     req.body = {};
@@ -91,9 +91,9 @@ describe('Authentication using TOTP', function () {
                     req.body.password = PASSWORD;
                     req.body.code = '123';
                 })
-                .authenticate();    
+                .authenticate();
         });
-        
+
         it('check error message', function () {
             expect(info).to.be.equal(AUTH_FAILED);
         });
@@ -102,10 +102,10 @@ describe('Authentication using TOTP', function () {
 });
 
 describe('Authentication without TOTP', function () {
-    
+
     describe ('pass request to the callbacks', function () {
-        
-        var strategy = new Strategy({
+
+        const strategy = new Strategy({
             skipTotpVerification: true,
             passReqToCallback: true
         }, function (req, username, password, done) {
@@ -115,60 +115,60 @@ describe('Authentication without TOTP', function () {
                 done(null, null, { error: AUTH_FAILED, req: req });
             }
         });
-        
+
         describe ('authentication - OK', function () {
-            var user;
-                
+            let user;
+
             before(function (done) {
                 chai.passport.use(strategy)
                     .success(function (u) {
                         user = u;
-                        done();    
+                        done();
                     })
                     .req(function (req) {
                         req.body = {};
                         req.body.username = USERNAME;
                         req.body.password = PASSWORD;
                     })
-                    .authenticate();    
+                    .authenticate();
             });
-            
+
             it('check username', function () {
                 expect(user.username).to.be.equal(USERNAME);
                 expect(user.req.body.username).to.be.equal(USERNAME);
                 expect(user.req.body.password).to.be.equal(PASSWORD);
             });
         });
-        
+
         describe ('authentication - failed', function () {
-            var info;
-                
+            let info;
+
             before(function (done) {
                 chai.passport.use(strategy)
                     .fail(function (i) {
                         info = i;
-                        done();    
+                        done();
                     })
                     .req(function (req) {
                         req.body = {};
                         req.body.username = USERNAME;
                         req.body.password = WRONG_PASSWORD;
                     })
-                    .authenticate();    
+                    .authenticate();
             });
-            
+
             it('check error message', function () {
                 expect(info.error).to.be.equal(AUTH_FAILED);
                 expect(info.req.body.username).to.be.equal(USERNAME);
                 expect(info.req.body.password).to.be.equal(WRONG_PASSWORD);
             });
         });
-        
+
     });
-    
+
     describe ('do not pass request to the callbacks', function () {
-        
-        var strategy = new Strategy({
+
+        const strategy = new Strategy({
             skipTotpVerification: true
         }, function (username, password, done) {
             if (username === ERROR_USERNAME) {
@@ -180,115 +180,114 @@ describe('Authentication without TOTP', function () {
                 done(null, null, AUTH_FAILED);
             }
         });
-        
+
         describe ('authentication - OK', function () {
-            var user;
-                
+            let user;
+
             before(function (done) {
                 chai.passport.use(strategy)
                     .success(function (u) {
                         user = u;
-                        done();    
+                        done();
                     })
                     .req(function (req) {
                         req.body = {};
                         req.body.username = USERNAME;
                         req.body.password = PASSWORD;
                     })
-                    .authenticate();    
+                    .authenticate();
             });
-            
+
             it('check username', function () {
                 expect(user.username).to.be.equal(USERNAME);
             });
         });
-        
+
         describe ('authentication - failed', function () {
-            var info;
-                
+            let info;
+
             before(function (done) {
                 chai.passport.use(strategy)
                     .fail(function (i) {
                         info = i;
-                        done();    
+                        done();
                     })
                     .req(function (req) {
                         req.body = {};
                         req.body.username = USERNAME;
                         req.body.password = WRONG_PASSWORD;
                     })
-                    .authenticate();    
+                    .authenticate();
             });
-            
+
             it('check error message', function () {
                 expect(info).to.be.equal(AUTH_FAILED);
             });
         });
-        
+
         describe ('authentication - error', function () {
-            var error;
-                
+            let error;
+
             before(function (done) {
                 chai.passport.use(strategy)
                     .error(function (e) {
                         error = e;
-                        done();    
+                        done();
                     })
                     .req(function (req) {
                         req.body = {};
                         req.body.username = ERROR_USERNAME;
                         req.body.password = WRONG_PASSWORD;
                     })
-                    .authenticate();    
+                    .authenticate();
             });
-            
+
             it('check error message', function () {
                 expect(error).to.be.equal(ERROR);
             });
         });
-        
+
     });
 
 });
 
 describe('Authentication with missign credentials', function () {
-    
-    var strategy = new Strategy(function () {}, function () {});
-    
+    const strategy = new Strategy(function () {}, function () {});
+
     describe('default error message', function () {
-        var info;
-        
+        let info;
+
         before(function (done) {
             chai.passport.use(strategy)
                 .fail(function (message) {
                     info = message;
-                    done();    
+                    done();
                 })
                 .authenticate();
         });
-        
+
         it('should be default missing credentials message', function () {
-            expect(info).to.be.equal('Missing credentials');    
-        });    
+            expect(info).to.be.equal('Missing credentials');
+        });
     });
-    
+
     describe('custom error message', function () {
-        var info;
-        
+        let info;
+
         before(function (done) {
             chai.passport.use(strategy)
                 .fail(function (message) {
                     info = message;
-                    done();    
+                    done();
                 })
                 .authenticate({
                     badRequestMessage: 'Failed'
                 });
         });
-        
+
         it('should be default missing credentials message', function () {
-            expect(info).to.be.equal('Failed');    
-        });   
+            expect(info).to.be.equal('Failed');
+        });
     });
-    
+
 });
